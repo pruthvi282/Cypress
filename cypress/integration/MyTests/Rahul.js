4306e6b4ac1f124cd716fb587f9a2eed3a19cfd2
pruthvi282 <pruthvi282@gmail.com>
2022-03-21 23:55:37 +0530

    test

--- cypress/integration/MyTests/Rahul.js
+++ cypress/integration/MyTests/Rahul.js
@@ -1,19 +1,19 @@
-describe('My TestSuite',function(){
-    it('My first TC',function(){
+describe('My TestSuite', function () {
+    it('My first TC', function () {
         cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
-        cy.get('#checkBoxOption1').check().should('be.checked').and('have.value','option1');
+        cy.get('#checkBoxOption1').check().should('be.checked').and('have.value', 'option1');
         cy.get('label>input[type="checkbox"]').check(['option2'])
 
         //Select dropdown
-        cy.get('select').select('Option3').should('have.value','option3')
+        cy.get('select').select('Option3').should('have.value', 'option3')
 
         //dynamic
         cy.get('#autocomplete').type('ind')
-        cy.get('.ui-menu-item > div').each(($el, index,$list)=>{
-          if($el.text().includes('British Indian Ocean Territory')){
-        cy.wrap($el).click();
+        cy.get('.ui-menu-item > div').each(($el, index, $list) => {
+            if ($el.text().includes('British Indian Ocean Territory')) {
+                cy.wrap($el).click();
 
-          }
+            }
         })
         cy.get('#displayed-text').should('be.visible')
         cy.get('#hide-textbox').click();
@@ -21,4 +21,4 @@ describe('My TestSuite',function(){
         cy.get('[value="radio1"]').check().should('be.checked')
 
     })
-}) 
+})
